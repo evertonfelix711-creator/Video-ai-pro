93b4a9a079ac7af603640a8775760b2d13b94677
evertonfelix711-creator <evertonfelix711@gmail.com>
2026-01-14 18:31:06 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 const Index = () => {
   return (
-    retornar (
   <div nomeDaClasse="min-h-screen bg-background">
     <Barra de navegação />
     <Herói />
