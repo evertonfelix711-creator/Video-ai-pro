c0cd7781b6bb6149b2d15f5b612e0970b1b54f16
evertonfelix711-creator <evertonfelix711@gmail.com>
2026-01-14 18:28:57 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,23 @@
 import { Navbar } from "../components/Navbar";
 import { Hero } from "../components/Hero";
+import { Link } from "react-router-dom";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-background">
-      <Navbar />
-      <Hero />
+    retornar (
+  <div nomeDaClasse="min-h-screen bg-background">
+    <Barra de navegação />
+    <Herói />
+
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <Link to="/pricing">
+        <button style={{ padding: "12px 24px", fontSize: "16px" }}>
+          Ver Planos e Preços
+        </button>
+      </Link>
     </div>
-  );
-};
+
+  </div>
+);
 
 export default Index;
