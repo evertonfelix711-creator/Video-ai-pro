a9500a5d50a2f607682d8a1984a9ce528e2c4d64
evertonfelix711-creator <evertonfelix711@gmail.com>
2026-01-14 18:14:35 -0300

    Update App.tsx
    ediçoes 6

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Create from "./pages/Create";
 import Gallery from "./pages/Gallery";
-import Pricing from "./pages/Pricing";
+import Pricing from "./pages/pricing";
 import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 const App = () => (
